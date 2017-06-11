3cb34f60e285331d5ce359e1b9691a6839cdb757
BilalAM <bilal.asif.97@gmail.com>
2017-06-11 12:57:18 +0500

    first commit done

---
+++ src/main/java/knn/IOUtility.java
@@ -0,0 +1,40 @@
+package knn;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/* File IO Utility Class */
+public class IOUtility {
+
+	private static final Path filePath = Paths.get("/mining_craze/datasets/knn_dataset.txt");
+	private static List<String> fileLines = new ArrayList<String>();
+
+	
+
+	
+	
+	
+	
+	
+	public static double arbitraryEucledianDistance(double[] upperPoints, double[] lowerPoints) {
+		double result = 0;
+		if (upperPoints.length == lowerPoints.length) {
+			for (int i = 0; i < upperPoints.length; i++) {
+				result += doublePointsProduct(upperPoints[i], lowerPoints[i]);
+			}
+		} else {
+			System.out.println("group points are not equal");
+			return 0.0;
+		}
+		return Math.sqrt(result);
+	}
+
+	private static double doublePointsProduct(double x1, double x2) {
+		return (Math.pow(((x1 - x2)), 2));
+	}
+}
---
+++ src/main/java/knn/test.java
@@ -0,0 +1,12 @@
+package knn;
+
+public class test {
+
+	public static void main(String[] args) {
+
+		System.out.println(IOUtility.arbitraryEucledianDistance(new double[] { 48, 142000, 180.7 },
+				new double[] { 33, 150000, 2640 }));
+		
+		
+	}
+}
