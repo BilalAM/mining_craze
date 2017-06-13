3c7429907dab908e94095bd95aa70a5c5d2747c5
BilalAM <bilal.asif.97@gmail.com>
2017-06-13 23:18:49 +0500

    major changes

--- src/main/java/knn/test.java
+++ src/main/java/knn/test.java
@@ -2,12 +2,16 @@ package knn;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
 public class test {
 
 	public static void main(String[] args) throws Exception {
-
-		System.out.println(IOUtility.averageOfDistances(new double[] { 2, 1, 3, 4 }));
-
+		IOUtility util = new IOUtility();
+		for(Double d : IOUtility.getDistances(10, 3)){
+			System.out.println(d);
+		}
+		System.out.println("hello");
 	}
 }
