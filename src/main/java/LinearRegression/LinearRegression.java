00185d24e8d49c241835042d165176f497a4b809
BilalAM <bilal.asif.97@gmail.com>
2017-06-21 21:15:03 +0500

    formula attempt 1

--- src/main/java/LinearRegression/LinearRegression.java
+++ src/main/java/LinearRegression/LinearRegression.java
@@ -1,5 +1,22 @@
 package LinearRegression;
 
-public class LinearRegression {
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class LinearRegression {
+	private static Path path = Paths.get("/datasets/linear_regression.txt");
+	
+	
+	public Double calculateValue(double independantValue){
+		double y = calculateA() + calcualteB() * independantValue;
+		return y;
+	}
+	
+	private static double calcualteB(){
+		return 0.0;
+	}
+	private static double calculateA(){
+		return 0.0;
+	}
+	
 }
