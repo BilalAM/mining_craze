dabe6a987f3958d7b486443a655892248a7c9ec1
BilalAM <bilal.asif.97@gmail.com>
2017-06-21 21:15:40 +0500

    utils

--- src/main/java/LinearRegression/LinearRegressionUtils.java
+++ src/main/java/LinearRegression/LinearRegressionUtils.java
@@ -1,5 +1,18 @@
 package LinearRegression;
 
-public class LinearRegressionUtils {
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
+public class LinearRegressionUtils {
+	/* taken from knn.KnnUtils class*/
+	public static double averageOfPoints(Double...values) {
+		double product = 0.0;
+		for (double d : values) {
+			product += (d / values.length);
+		}
+		return product;
+	}
+	
+	
 }
