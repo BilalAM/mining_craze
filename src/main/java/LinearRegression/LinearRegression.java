61240232d32b24420d3dc3426ce514b297e5b143
BilalAM <bilal.asif.97@gmail.com>
2017-06-21 23:35:08 +0500

    dependant values method

--- src/main/java/LinearRegression/LinearRegression.java
+++ src/main/java/LinearRegression/LinearRegression.java
@@ -1,22 +1,41 @@
 package LinearRegression;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class LinearRegression {
-	private static Path path = Paths.get("/datasets/linear_regression.txt");
-	
-	
-	public Double calculateValue(double independantValue){
+	private static Path path = Paths.get("datasets/linear_regression");
+
+	public Double calculateValue(double independantValue) {
 		double y = calculateA() + calcualteB() * independantValue;
 		return y;
 	}
-	
-	private static double calcualteB(){
+
+	private static double calcualteB() {
+		
+		
 		return 0.0;
 	}
-	private static double calculateA(){
+
+	private static double calculateA() {
 		return 0.0;
 	}
-	
+
+	public static List<Double> getDependantValues() throws Exception {
+		List<Double> list = new ArrayList<>();
+		String line;
+		try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
+			while ((line = reader.readLine()) != null) {
+				String[] values = line.split(",");
+				list.add(Double.parseDouble(values[0]));
+			}
+		}
+		return list;
+	}
+
 }
