794bb20ed72a0e874f1da24fd98899a0f2b75245
BilalAM <bilal.asif.97@gmail.com>
2017-06-24 21:05:46 +0500

    testing

--- src/main/java/LinearRegression/test.java
+++ src/main/java/LinearRegression/test.java
@@ -3,7 +3,7 @@ package LinearRegression;
 public class test {
 	public static void main(String[] args) {
 		try {
-			for (Double d : LinearRegression.getDependantValues()) {
+			for (Double d : LinearRegression.getIndependantValues()) {
 				System.out.println(d);
 			}
 		} catch (Exception e) {
