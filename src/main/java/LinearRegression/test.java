a4c253067f1cdc11fb12c160cc334bae8a77b001
BilalAM <bilal.asif.97@gmail.com>
2017-06-26 21:07:08 +0500

    testing calcualteA()

--- src/main/java/LinearRegression/test.java
+++ src/main/java/LinearRegression/test.java
@@ -2,13 +2,6 @@ package LinearRegression;
 
 public class test {
 	public static void main(String[] args) {
-		try {
-			for (Double d : LinearRegression.getIndependantValues()) {
-				System.out.println(d);
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.out.println(LinearRegression.calculateA());
 	}
 }
